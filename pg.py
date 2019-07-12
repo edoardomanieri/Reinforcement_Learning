36c3130b71f4be0c5e089753148d92d7dfe84296
edoardomanieri <edoardomanieri@gmail.com>
2019-07-12 12:48:42 +0200

    modified pg

--- pg.py
+++ pg.py
@@ -11,29 +11,24 @@ from keras.layers.core import Lambda
 import plotting
 
 EPISODES_TO_TRAIN = 5
+TOTAL_EPISODES = 20
+ENTROPY_BETA = 0.001
 
 env = gym.make("Taxi-v2").env
 
-def custom_loss(batch_qvals, batch_states):
+def custom_loss(batch_scales, batch_states):
 
     def loss(y_true, y_pred):
         log_prob = K.log(y_pred)[:len(batch_states),:]
+        entropy = -K.mean(K.sum(y_pred * log_prob, axis = 1))
+        entropy_loss = -ENTROPY_BETA * entropy
         argmax_flat = K.argmax(y_true, axis=1) + [env.action_space.n * _ for _ in range(len(batch_states))]
         log_prob = K.reshape(log_prob, (env.action_space.n*len(batch_states),))
         log_prob = K.gather(log_prob,argmax_flat)
-        log_prob_action = K.cast(batch_qvals, K.floatx()) * log_prob
-        return -K.mean(log_prob_action)
+        log_prob_action = K.cast(batch_scales, K.floatx()) * log_prob
+        return -K.mean(log_prob_action) + entropy_loss
 
     return loss
-
-def calc_qvals(rewards, gamma):
-    res = []
-    sum_r = 0.0
-    for r in reversed(rewards):
-        sum_r *= gamma
-        sum_r += r
-        res.append(sum_r)
-    return list(reversed(res))
 
 inputs = Input(shape=(1,))
 x = Dense(256, activation='relu', kernel_initializer='zero', kernel_regularizer=l2(0.001))(inputs)
@@ -46,7 +41,7 @@ step_idx = 0
 done_episodes = 0
 
 batch_episodes = 0
-batch_states, batch_actions, batch_qvals = [], [], []
+batch_states, batch_actions, batch_scales = [], [], []
 cur_rewards = []
 episodes = 20
 gamma = 1
@@ -64,7 +59,6 @@ for i in range(1, episodes):
     epochs, penalties, reward, = 0, 0, 0
     done = False
 
-
     while not done:
 
         action = np.random.choice([a for a in range(env.action_space.n)], p=model.predict([state]).squeeze())
@@ -73,18 +67,17 @@ for i in range(1, episodes):
         batch_states.append(state)
         batch_actions.append(action)
         cur_rewards.append(reward)
+        baseline = reward / i
+        batch_scales.append(reward - baseline)
 
         stats.episode_rewards[i] += reward
         stats.episode_lengths[i] = epochs
-
-        if reward == -10:
-            penalties += 1
 
         state = next_state
         epochs += 1
 
     print("episode", i)
-    batch_qvals.extend(calc_qvals(cur_rewards, gamma))
+
     cur_rewards.clear()
     batch_episodes += 1
 
@@ -92,11 +85,11 @@ for i in range(1, episodes):
         continue
 
     y = np_utils.to_categorical(batch_actions, env.action_space.n)
-    model.compile(optimizer = Adam(lr=0.0001), loss = custom_loss(batch_qvals, batch_states))
+    model.compile(optimizer = Adam(lr=0.0001), loss = custom_loss(batch_scales, batch_states))
     model.fit(x=np.array(batch_states).reshape(-1,1), y= np.array(y).reshape(-1,6), batch_size=len(batch_states), epochs=100, verbose = 0)
     batch_episodes = 0
     batch_states.clear()
     batch_actions.clear()
-    batch_qvals.clear()
+    batch_scales.clear()
 
 plotting.plot_episode_stats(stats)
