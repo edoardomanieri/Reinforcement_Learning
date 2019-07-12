fb3c74c3c12ef7072f7e35492a55adedd8eb66e3
edoardomanieri <edoardomanieri@gmail.com>
2019-07-12 23:14:08 +0200

    "pg applied to CartPole env"

--- cartpolepg.py
+++ cartpolepg.py
@@ -10,26 +10,25 @@ import keras.backend as K
 from keras.layers.core import Lambda
 import plotting
 
-EPISODES_TO_TRAIN = 2
-TOTAL_EPISODES = 200
+EPISODES_TO_TRAIN = 10
+TOTAL_EPISODES = 10000
 ENTROPY_BETA = 0.001
 
-env = gym.make("Taxi-v2").env
+env = gym.make("CartPole-v1").env
 
 def custom_loss(batch_scales, batch_states):
 
     def loss(y_true, y_pred):
         log_prob = K.log(y_pred)[:len(batch_states),:]
-        #entropy = -K.mean(K.sum(y_pred * log_prob, axis = 1))
-        #entropy_loss = -ENTROPY_BETA * entropy
+        entropy = -K.mean(K.sum(y_pred * log_prob, axis = 1))
+        entropy_loss = -ENTROPY_BETA * entropy
         argmax_flat = K.argmax(y_true, axis=1) + [env.action_space.n * _ for _ in range(len(batch_states))]
         log_prob = K.reshape(log_prob, (env.action_space.n*len(batch_states),))
         log_prob = K.gather(log_prob,argmax_flat)
         log_prob_action = K.cast(batch_scales, K.floatx()) * log_prob
-        return -K.sum(log_prob_action) #+ entropy_loss
+        return -K.mean(log_prob_action) + entropy_loss
 
     return loss
-
 
 def calc_qvals(rewards, gamma):
     res = []
@@ -41,7 +40,7 @@ def calc_qvals(rewards, gamma):
     return list(reversed(res))
 
 
-inputs = Input(shape=(1,))
+inputs = Input(shape=(env.observation_space.shape[0],))
 x = Dense(256, activation='relu' ,kernel_regularizer=l2(0.001))(inputs)
 predictions = Dense(env.action_space.n, activation='softmax', kernel_regularizer=l2(0.001))(x)
 
@@ -61,69 +60,59 @@ stats = plotting.EpisodeStats(
         episode_rewards=np.zeros(TOTAL_EPISODES))
 
 
-###understand and add baseline!!
 for i in range(1, TOTAL_EPISODES):
 
     state = env.reset()
-    state = state / env.observation_space.n
     epochs, penalties, reward, = 0, 0, 0
     done = False
     step_idx = 0
+    discounted_reward = 0
 
     while not done:
 
         step_idx += 1
-        action = np.random.choice([a for a in range(env.action_space.n)], p=model.predict([state]).squeeze())
+        action = np.random.choice([a for a in range(env.action_space.n)], p=model.predict(state.reshape(-1,env.observation_space.shape[0])).squeeze())
         next_state, reward, done, info = env.step(action)
-        next_state = next_state / env.observation_space.n
         batch_states.append(state)
         batch_actions.append(action)
+        discounted_reward *= gamma
+        discounted_reward += reward
 
         stats.episode_rewards[i] += reward
         stats.episode_lengths[i] = epochs
 
-        baseline = stats.episode_rewards[i] / step_idx
-        cur_rewards.append(reward - baseline)
-        #print(reward)
-        #print(reward - baseline)
+        #mettere il discount qua invece che alla fine dell episodio per calcolare la baseline
+        baseline = discounted_reward / step_idx
+        batch_scales.append(reward - baseline)
         state = next_state
         epochs += 1
 
-    print("episode", i)
-    batch_scales.extend(calc_qvals(cur_rewards, gamma))
+    if i % 100 == 0:
+        print("episode", i)
+
+    #batch_scales.extend(calc_qvals(cur_rewards, gamma))
     cur_rewards.clear()
     batch_episodes += 1
+
+    if i > 100 and i % 10 == 0:
+        print("mean last 100: ", np.mean(stats.episode_rewards[i-100:i]))
+        if np.mean(stats.episode_rewards[-100:]) >= 195:
+            print("solved")
+            break
 
     if batch_episodes < EPISODES_TO_TRAIN:
         continue
 
     y = np_utils.to_categorical(batch_actions, env.action_space.n)
-    model.compile(optimizer = Adam(lr=0.0001), loss = custom_loss(batch_scales, batch_states))
-    model.fit(x=np.array(batch_states).reshape(-1,1), y= np.array(y).reshape(-1,6), batch_size=len(batch_states), epochs=3, verbose = 1)
+    model.compile(optimizer = Adam(lr=0.001), loss = custom_loss(batch_scales, batch_states))
+    model.fit(x=np.array(batch_states).reshape(-1,env.observation_space.shape[0]), y= np.array(y).reshape(-1,env.action_space.n), batch_size=len(batch_states), epochs=2, verbose = 0)
     batch_episodes = 0
     batch_states.clear()
     batch_actions.clear()
     batch_scales.clear()
 
+
 plotting.plot_episode_stats(stats)
 
-model.predict([0.1,0.2,0.9])
-batch_scales
-
-batch_scales = [-1,-1,-1, 20]
-def loss(y_true, y_pred):
-    print(y_pred)
-    log_prob = np.log(y_pred)[:4,:]
-    print(log_prob)
-    argmax_flat = np.argmax(y_true, axis=1) + [env.action_space.n * _ for _ in range(4)]
-    log_prob = np.reshape(log_prob, (env.action_space.n*4,))
-    log_prob = log_prob[argmax_flat]
-    print(log_prob)
-    log_prob_action = batch_scales * log_prob
-    print(log_prob_action)
-    return -np.mean(log_prob_action)
-
-loss(np.array([[1,0,0,0,0,0],[1,0,0,0,0,0], [1,0,0,0,0,0], [0,0,0,0,0,1]]),model.predict([0.1,0.2,0.3, 0.5]))
-
-model.predict([0.1,0.2,0.3])
-batch_scales
+state
+model.predict(np.array([1,1,1,1]).reshape(-1,env.observation_space.shape[0])).squeeze()
